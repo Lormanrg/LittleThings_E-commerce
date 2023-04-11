a28d9bf5b564b41bbb9985d7733dc4e7d0992d56
Lorman Romero <lormanrg@gmail.com>
2023-04-10 21:18:41 -0400

    In process to build an e-commerce using mantine as a desing tool

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,14 +2,40 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import { Button } from "@mantine/core";
+import home from "../../styles/home.css";
+import { Button, Container, Flex, Text, Title } from "@mantine/core";
+import logo from "../../img/logo.jpg";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="text-center mt-5">
-      <Button>Button</Button>
-    </div>
+    <>
+      <Container bg="gray">
+        <Flex>
+          <img className="logo" width="100px" height="100px" src={logo}></img>
+        </Flex>
+      </Container>
+      <Container className="cover">
+        <Flex bg="gray">
+          <img className="logo" width="200px" height="200px" src={logo}></img>
+        </Flex>
+        <Flex bg="gray" className="title">
+          <Title order={2} align="center" fs="italic">
+            "Bienvenidos a nuestra tienda online donde encontraras los mejores
+            disenos personalizados a tu gusto con el mejor factor precio calidad
+            del mercado".{" "}
+          </Title>
+        </Flex>
+        <Flex>
+          <Link to="/menu">
+            <Button order={1} align="center" fs="italic" className="button">
+              "Presiona aqui para saber mas"
+            </Button>
+          </Link>
+        </Flex>
+      </Container>
+    </>
   );
 };
---
+++ src/front/js/pages/menu.jsx
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Menu = () => {
+  const { store, actions } = useContext(Context);
+
+  return <>Hola que tal JAJAJAJ</>;
+};
