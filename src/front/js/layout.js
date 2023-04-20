626146a05a8f74ac6ecb693433c25423b70e843a
Lorman Romero <lormanrg@gmail.com>
2023-04-20 16:42:01 -0400

    many changes have been applied

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { BackendURL } from "./component/backendURL";
 import { MantineProvider } from "@mantine/core";
 import { Home } from "./pages/home.jsx";
 import { Menu } from "./pages/menu.jsx";
-
 import injectContext from "./store/appContext";
 
 //create your first component
@@ -14,9 +11,6 @@ const Layout = () => {
   // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
 
-  if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "")
-    return <BackendURL />;
-
   return (
     <div>
       <MantineProvider withGlobalStyles withNormalizeCSS>
@@ -24,6 +18,7 @@ const Layout = () => {
           <Routes>
             <Route element={<Home />} path="/" />
             <Route element={<Menu />} path="/menu" />
+
             <Route element={<h1>Not found!</h1>} />
           </Routes>
         </BrowserRouter>
