f948b226e2587ea2899770e8b899a8de356d01f1
Lorman Romero <lormanrg@gmail.com>
2023-04-23 00:07:09 -0400

    building a navbar

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,22 +1,52 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Button, Flex, Container, Menu, Text } from "@mantine/core";
 import { Link } from "react-router-dom";
-import { Button, Flex, Container } from "@mantine/core";
 import logo from "../../img/logo.jpg";
-
+import "../../styles/content.css";
+import { Activity } from "tabler-icons-react";
 export const Navbar = () => {
-  const { store, actions } = useContext(Context);
+  const { store, context } = useContext(Context);
 
   return (
     <>
-      <Container bg="gray" display="flex">
-        <Flex>
-          <img className="logo" width="100px" height="100px" src={logo}></img>
-        </Flex>
-        <Flex gap="lg" align="flex-start" justify="flex-end">
-          <Button color="teal">Register</Button>
+      {" "}
+      <Container
+        className="content"
+        bg="gray"
+        display="flex"
+        justify="space-between"
+      >
+        <Link to="/">
+          <Flex>
+            <img className="logo" width="100px" height="100px" src={logo}></img>
+          </Flex>
+        </Link>
+        <Flex
+          mih={50}
+          bg="gray"
+          gap="xs"
+          justify="flex-end"
+          align="center"
+          direction="row"
+          wrap="wrap"
+        >
+          <Button color="blue">Register</Button>
           <Button color="teal">Login</Button>
-          <Button color="teal">Marketplace</Button>
+          <Menu trigger="hover" openDelay={100} closeDelay={400}>
+            <Menu.Target>
+              <Button>Marketplace</Button>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>Categorías</Menu.Label>
+              <Link to="/t-shirts">
+                <Menu.Item icon={<Activity size={14} />}>T-shirts</Menu.Item>
+              </Link>
+              <Menu.Item icon={<Activity size={14} />}>Perfumes</Menu.Item>
+              <Menu.Item icon={<Activity size={14} />}>Accesorios</Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
           <Button color="gray">Carrito de compras</Button>
         </Flex>
       </Container>
