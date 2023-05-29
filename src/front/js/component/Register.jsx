4b3a3032b8e7a4b759865eb7eff2eb5d906e600c
Lorman Romero <lormanrg@gmail.com>
2023-05-29 17:19:43 -0400

    building register

--- src/front/js/component/Register.jsx
+++ src/front/js/component/Register.jsx
@@ -11,15 +11,62 @@ import {
   Container,
   Menu,
   Text,
+  Box,
+  Checkbox,
 } from "@mantine/core";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useForm } from "@mantine/form";
 
 const Register = () => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      termsOfService: false,
+    },
+
+    validate: {
+      username: (value) => (value.trim() !== "" ? null : "Invalid username"),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => (value.trim() !== "" ? null : "Invalid password"),
+    },
+  });
   return (
     <>
-      <h1>Hola que tal </h1>
+      <Box maw={400} mx="auto">
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <TextInput
+            withAsterisk
+            label="Username"
+            placeholder="Nombre de usuario"
+            {...form.getInputProps("username")}
+          />
+
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="Correo"
+            {...form.getInputProps("email")}
+          />
+
+          <TextInput
+            withAsterisk
+            label="Password"
+            placeholder="Password"
+            {...form.getInputProps("password")}
+          />
+
+          <Checkbox
+            mt="md"
+            label="I agree to sell my privacy"
+            {...form.getInputProps("termsOfService", { type: "checkbox" })}
+          />
+
+          <Group position="right" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
+      </Box>
     </>
   );
 };
